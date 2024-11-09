0838aa11be10078b109a7cfa324a2c721ce380c7
Ahsan Iqbal <150703987+ahsan-iqbaldev@users.noreply.github.com>
2024-11-10 02:12:57 +0500

    Handle auth integration using appwrite

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -6,7 +6,6 @@ import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -16,10 +15,8 @@ import { Input } from "@/components/ui/input";
 import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
-
-const formSchema = z.object({
-  username: z.string().min(2).max(50),
-});
+import { createAccount, signInUser } from "@/lib/actions/user.action";
+import OtpModal from "./OTPModal";
 
 type formType = "sign-in" | "sign-up";
 
@@ -52,111 +49,121 @@ const AuthForm = ({ type }: { type: formType }) => {
     setIsLoading(true);
     setErrorMessage("");
 
-    // try {
-    //   const user =
-    //     type === "sign-up"
-    //       ? await createAccount({
-    //           fullName: values.fullName || "",
-    //           email: values.email,
-    //         })
-    //       : await signInUser({ email: values.email });
+    try {
+      const user =
+        type === "sign-up"
+          ? await createAccount({
+              fullName: values.fullName || "",
+              email: values.email,
+            })
+          : await signInUser({ email: values.email });
 
-    //   setAccountId(user.accountId);
-    // } catch {
-    //   setErrorMessage("Failed to create account. Please try again.");
-    // } finally {
-    //   setIsLoading(false);
-    // }
+      console.log({ user });
+      if (user.accountId) {
+        setAccountId(user.accountId);
+      } else {
+        setErrorMessage(user.error);
+      }
+    } catch {
+      setErrorMessage("Failed to create account. Please try again.");
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   return (
-    <Form {...form}>
-    <form onSubmit={form.handleSubmit(onSubmit)} className="auth-form">
-      <h1 className="form-title">
-        {type === "sign-in" ? "Sign In" : "Sign Up"}
-      </h1>
-      {type === "sign-up" && (
-        <FormField
-          control={form.control}
-          name="fullName"
-          render={({ field }) => (
-            <FormItem>
-              <div className="shad-form-item">
-                <FormLabel className="shad-form-label">Full Name</FormLabel>
+    <>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="auth-form">
+          <h1 className="form-title">
+            {type === "sign-in" ? "Sign In" : "Sign Up"}
+          </h1>
+          {type === "sign-up" && (
+            <FormField
+              control={form.control}
+              name="fullName"
+              render={({ field }) => (
+                <FormItem>
+                  <div className="shad-form-item">
+                    <FormLabel className="shad-form-label">Full Name</FormLabel>
 
-                <FormControl>
-                  <Input
-                    placeholder="Enter your full name"
-                    className="shad-input"
-                    {...field}
-                  />
-                </FormControl>
-              </div>
+                    <FormControl>
+                      <Input
+                        placeholder="Enter your full name"
+                        className="shad-input"
+                        {...field}
+                      />
+                    </FormControl>
+                  </div>
 
-              <FormMessage className="shad-form-message" />
-            </FormItem>
+                  <FormMessage className="shad-form-message" />
+                </FormItem>
+              )}
+            />
           )}
-        />
-      )}
 
-      <FormField
-        control={form.control}
-        name="email"
-        render={({ field }) => (
-          <FormItem>
-            <div className="shad-form-item">
-              <FormLabel className="shad-form-label">Email</FormLabel>
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <div className="shad-form-item">
+                  <FormLabel className="shad-form-label">Email</FormLabel>
 
-              <FormControl>
-                <Input
-                  placeholder="Enter your email"
-                  className="shad-input"
-                  {...field}
-                />
-              </FormControl>
-            </div>
+                  <FormControl>
+                    <Input
+                      placeholder="Enter your email"
+                      className="shad-input"
+                      {...field}
+                    />
+                  </FormControl>
+                </div>
 
-            <FormMessage className="shad-form-message" />
-          </FormItem>
-        )}
-      />
-
-      <Button
-        type="submit"
-        className="form-submit-button"
-        disabled={isLoading}
-      >
-        {type === "sign-in" ? "Sign In" : "Sign Up"}
-
-        {isLoading && (
-          <Image
-            src="/assets/icons/loader.svg"
-            alt="loader"
-            width={24}
-            height={24}
-            className="ml-2 animate-spin"
+                <FormMessage className="shad-form-message" />
+              </FormItem>
+            )}
           />
-        )}
-      </Button>
 
-      {errorMessage && <p className="error-message">*{errorMessage}</p>}
+          <Button
+            type="submit"
+            className="form-submit-button"
+            disabled={isLoading}
+          >
+            {type === "sign-in" ? "Sign In" : "Sign Up"}
 
-      <div className="body-2 flex justify-center">
-        <p className="text-light-100">
-          {type === "sign-in"
-            ? "Don't have an account?"
-            : "Already have an account?"}
-        </p>
-        <Link
-          href={type === "sign-in" ? "/signup" : "/signin"}
-          className="ml-1 font-medium text-brand"
-        >
-          {" "}
-          {type === "sign-in" ? "Sign Up" : "Sign In"}
-        </Link>
-      </div>
-    </form>
-  </Form>
+            {isLoading && (
+              <Image
+                src="/assets/icons/loader.svg"
+                alt="loader"
+                width={24}
+                height={24}
+                className="ml-2 animate-spin"
+              />
+            )}
+          </Button>
+
+          {errorMessage && <p className="error-message">{errorMessage}</p>}
+
+          <div className="body-2 flex justify-center">
+            <p className="text-light-100">
+              {type === "sign-in"
+                ? "Don't have an account?"
+                : "Already have an account?"}
+            </p>
+            <Link
+              href={type === "sign-in" ? "/signup" : "/signin"}
+              className="ml-1 font-medium text-brand"
+            >
+              {" "}
+              {type === "sign-in" ? "Sign Up" : "Sign In"}
+            </Link>
+          </div>
+        </form>
+      </Form>
+      {accountId && (
+        <OtpModal email={form.getValues("email")} accountId={accountId} />
+      )}
+    </>
   );
 };
 
---
+++ lib/actions/user.action.ts
@@ -0,0 +1,112 @@
+"use server";
+
+import { ID, Query } from "node-appwrite";
+import { createAdminClient } from "../appwrite";
+import { appwriteConfig } from "../appwrite/config";
+import { avatarPlaceholderUrl } from "@/constants";
+import { cookies } from "next/headers";
+import { parseStringify } from "../utils";
+
+////Get Document using email
+const getUserByEmail = async (email: string) => {
+  const { databases } = await createAdminClient();
+  const result = await databases.listDocuments(
+    appwriteConfig.databaseId,
+    appwriteConfig.usersCollectionId,
+    [Query.equal("email", [email])]
+  );
+
+  return result.total > 0 ? result.documents[0] : null;
+};
+
+////handle Error Function
+const handleError = (error: unknown, message: string) => {
+  console.log(error, message);
+  throw error;
+};
+
+///Send otp on email function
+export const sendEmailOTP = async ({ email }: { email: string }) => {
+  const { account } = await createAdminClient();
+
+  try {
+    const session = await account.createEmailToken(ID.unique(), email);
+
+    return session.userId;
+  } catch (error) {
+    handleError(error, "Failed to send email OTP");
+  }
+};
+
+////Create User
+export const createAccount = async ({
+  fullName,
+  email,
+}: {
+  fullName: string;
+  email: string;
+}) => {
+  const existingUser = await getUserByEmail(email);
+  const accountId = await sendEmailOTP({ email });
+  if (!accountId) throw new Error("Failed to send an OTP");
+
+  if (!existingUser) {
+    const { databases } = await createAdminClient();
+
+    await databases.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.usersCollectionId,
+      ID.unique(),
+      {
+        fullName,
+        email,
+        avatar: avatarPlaceholderUrl,
+        accountId,
+      }
+    );
+  }
+  return parseStringify({ accountId });
+};
+
+/// verify otp function
+export const verifySecret = async ({
+  accountId,
+  password,
+}: {
+  accountId: string;
+  password: string;
+}) => {
+  try {
+    const { account } = await createAdminClient();
+
+    const session = await account.createSession(accountId, password);
+
+    (await cookies()).set("appwrite-session", session.secret, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "strict",
+      secure: true,
+    });
+
+    return parseStringify({ sessionId: session.$id });
+  } catch (error) {
+    handleError(error, "Failed to verify OTP");
+  }
+};
+
+//// signin user
+
+export const signInUser = async ({ email }: { email: string }) => {
+  try {
+    const existingUser = await getUserByEmail(email);
+
+    if (existingUser) {
+      await sendEmailOTP({ email });
+      return parseStringify({ accountId: existingUser.accountId });
+    }
+
+    return parseStringify({ accountId: null, error: "User not found" });
+  } catch (error) {
+    handleError(error, "Failed to sign in user");
+  }
+};
